76200233fd2ac0f55d1dc4879e63c597bbc3fd9c
osqq <osqq95@gmail.com>
2018-09-23 21:13:19 +0300

    snap to place test added to snap

--- Assets/VRTK/Examples/ExampleResources/Scripts/SnapDropZoneGroup_Switcher.cs
+++ Assets/VRTK/Examples/ExampleResources/Scripts/SnapDropZoneGroup_Switcher.cs
@@ -6,11 +6,15 @@
     {
         private GameObject cubeZone;
         private GameObject sphereZone;
+        private GameObject intakeZone;
+        private GameObject valveZone;
 
         private void Start()
         {
             cubeZone = transform.Find("Cube_SnapDropZone").gameObject;
             sphereZone = transform.Find("Sphere_SnapDropZone").gameObject;
+            intakeZone = transform.Find("intake_snap").gameObject;
+            valveZone = transform.Find("valve_cover_snap").gameObject;
 
             cubeZone.GetComponent<VRTK_SnapDropZone>().ObjectEnteredSnapDropZone += new SnapDropZoneEventHandler(DoCubeZoneSnapped);
             cubeZone.GetComponent<VRTK_SnapDropZone>().ObjectSnappedToDropZone += new SnapDropZoneEventHandler(DoCubeZoneSnapped);
@@ -21,6 +25,16 @@
             sphereZone.GetComponent<VRTK_SnapDropZone>().ObjectSnappedToDropZone += new SnapDropZoneEventHandler(DoSphereZoneSnapped);
             sphereZone.GetComponent<VRTK_SnapDropZone>().ObjectExitedSnapDropZone += new SnapDropZoneEventHandler(DoSphereZoneUnsnapped);
             sphereZone.GetComponent<VRTK_SnapDropZone>().ObjectUnsnappedFromDropZone += new SnapDropZoneEventHandler(DoSphereZoneUnsnapped);
+
+            intakeZone.GetComponent<VRTK_SnapDropZone>().ObjectEnteredSnapDropZone += new SnapDropZoneEventHandler(DoIntakeZoneSnapped);
+            intakeZone.GetComponent<VRTK_SnapDropZone>().ObjectSnappedToDropZone += new SnapDropZoneEventHandler(DoIntakeZoneSnapped);
+            intakeZone.GetComponent<VRTK_SnapDropZone>().ObjectExitedSnapDropZone += new SnapDropZoneEventHandler(DoIntakeZoneUnsnapped);
+            intakeZone.GetComponent<VRTK_SnapDropZone>().ObjectUnsnappedFromDropZone += new SnapDropZoneEventHandler(DoIntakeZoneUnsnapped);
+
+            valveZone.GetComponent<VRTK_SnapDropZone>().ObjectEnteredSnapDropZone += new SnapDropZoneEventHandler(DoValveZoneSnapped);
+            valveZone.GetComponent<VRTK_SnapDropZone>().ObjectSnappedToDropZone += new SnapDropZoneEventHandler(DoValveZoneSnapped);
+            valveZone.GetComponent<VRTK_SnapDropZone>().ObjectExitedSnapDropZone += new SnapDropZoneEventHandler(DoValveZoneUnsnapped);
+            valveZone.GetComponent<VRTK_SnapDropZone>().ObjectUnsnappedFromDropZone += new SnapDropZoneEventHandler(DoValveZoneUnsnapped);
         }
 
         private void DoCubeZoneSnapped(object sender, SnapDropZoneEventArgs e)
@@ -39,6 +53,26 @@
         }
 
         private void DoSphereZoneUnsnapped(object sender, SnapDropZoneEventArgs e)
+        {
+            cubeZone.SetActive(true);
+        }
+
+        private void DoIntakeZoneSnapped(object sender, SnapDropZoneEventArgs e)
+        {
+            cubeZone.SetActive(false);
+        }
+
+        private void DoIntakeZoneUnsnapped(object sender, SnapDropZoneEventArgs e)
+        {
+            cubeZone.SetActive(true);
+        }
+
+        private void DoValveZoneSnapped(object sender, SnapDropZoneEventArgs e)
+        {
+            cubeZone.SetActive(false);
+        }
+
+        private void DoValveZoneUnsnapped(object sender, SnapDropZoneEventArgs e)
         {
             cubeZone.SetActive(true);
         }
