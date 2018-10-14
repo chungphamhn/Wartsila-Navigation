fa7efa364b182e81ae78a6be5f12ac7dc1898acd
osqq <osqq95@gmail.com>
2018-10-14 18:30:39 +0300

    show panel when pointing it at scene 19 + lever model

---
+++ Assets/ShowInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class ShowInfo : VRTK_InteractableObject
+{
+
+    //float spinSpeed = 0f;
+    //Transform rotator;
+    public GameObject UI;
+
+
+
+    public override void StartUsing(VRTK_InteractUse usingObject)
+    {
+        base.StartUsing(usingObject);
+        //spinSpeed = 360f;
+        UI.SetActive(true);
+    }
+
+    public override void StopUsing(VRTK_InteractUse usingObject)
+    {
+        base.StopUsing(usingObject);
+        //spinSpeed = 0f;
+        UI.SetActive(false);
+    }
+
+    protected void Start()
+    {
+        //rotator = transform.Find("Capsule");
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        //rotator.transform.Rotate(new Vector3(spinSpeed * Time.deltaTime, 0f, 0f));
+    }
+
+
+}
