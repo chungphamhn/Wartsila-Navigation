8f1ef986763a54b10dc0dcef63aff9cdf00e19e3
Lasse Pouru <lasse.pouru@pp.inet.fi>
2018-10-19 16:01:17 +0300

    Valve snap

---
+++ Assets/Scripts/ValveSnap.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using VRTK;
+
+public class ValveSnap : MonoBehaviour {
+
+    private VRTK_SnapDropZone dropZone;
+    [SerializeField] private GameObject rotatableValve;
+    
+    void Start ()
+    {
+        dropZone = GetComponent<VRTK_SnapDropZone>();
+        dropZone.ObjectSnappedToDropZone += EnableRotatableValve;
+    }
+
+    // disable snapped object and drop zone, enable rotatable valve
+    private void EnableRotatableValve(object sender, SnapDropZoneEventArgs e)
+    {
+        rotatableValve.SetActive(true);
+	dropZone.GetCurrentSnappedObject().SetActive(false);
+        this.gameObject.SetActive(false);
+    }
+}
