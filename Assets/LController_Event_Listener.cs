75aa0260ebd6105f552b0d69aa0e61dd679e4311
ToniOjanpera <toni.ojanpera@edu.turkuamk.fi>
2018-10-20 17:23:09 +0300

    added  left controller UI which opens and closes when touchpad is pressed (buttons still need functionality)

---
+++ Assets/LController_Event_Listener.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class LController_Event_Listener : MonoBehaviour {
+
+    public GameObject mainMenuCanvas;
+
+    private void Start()
+    {
+        if (GetComponent<VRTK_ControllerEvents>() == null)
+        {
+            VRTK_Logger.Error(VRTK_Logger.GetCommonMessage(VRTK_Logger.CommonMessageKeys.REQUIRED_COMPONENT_MISSING_FROM_GAMEOBJECT, "VRTK_ControllerEvents_ListenerExample", "VRTK_ControllerEvents", "the same"));
+            return;
+        }
+        GetComponent<VRTK_ControllerEvents>().TouchpadPressed += LController_Event_Listener_TouchpadPressed;
+    }
+
+    private void LController_Event_Listener_TouchpadPressed(object sender, ControllerInteractionEventArgs e)
+    {
+        mainMenuCanvas.SetActive(!mainMenuCanvas.activeSelf);
+    }
+}
