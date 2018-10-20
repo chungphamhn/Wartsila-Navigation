546cb394862605c3a848fbd736b2b1ea36b4e927
osqq <osqq95@gmail.com>
2018-10-21 01:43:02 +0300

    door can now be opened after valve is rotated given amount

---
+++ Assets/OpenDoor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour {
+
+    public GameObject valve;
+    private Rigidbody door;
+    public float rotation;
+
+
+	// Use this for initialization
+	void Start () {
+
+        door = GetComponent<Rigidbody>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Debug.Log(valve.transform.localRotation.eulerAngles.x >= 100);
+
+        if(valve.transform.localRotation.eulerAngles.x >= 100)
+        {
+
+            //can open door
+            door.isKinematic = false;
+            
+        }
+		
+	}
+}
