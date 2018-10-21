e00056cffbccee1e0006045cfa7b434be2ee863c
osqq <osqq95@gmail.com>
2018-10-21 19:30:00 +0300

    timer that starts when player walks trough the door

---
+++ Assets/Timer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    private float timer;
+    public Text clock;
+    private bool timerStarted;
+    private 
+
+	// Use this for initialization
+	void Start () {
+        timer = 0.0f;
+        timerStarted = false;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (timerStarted)
+        {
+            timer += Time.deltaTime;
+        }
+
+        string minutes = Mathf.Floor(timer / 60).ToString("00");
+        string seconds = (timer % 60).ToString("00");
+
+        clock.text = minutes + ":" + seconds;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        timerStarted = true;   
+    }
+}
